4e05a4e699241e0819426486c4e0e2d79d0ca40e
JACK <JACK@JACK-PC>
2018-03-23 15:16:26 +0800

    init

---
+++ abc/src/main/java/com/lg/test/AbcApplication.java
@@ -0,0 +1,12 @@
+package com.lg.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcApplication.class, args);
+	}
+}
